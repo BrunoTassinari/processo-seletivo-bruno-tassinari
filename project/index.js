b435b47fc558e0a806d658f4913c50c3f0cd2210
Bruno <bruno.fagundes80@gmail.com>
2022-03-26 14:25:05 -0300

    code fisinh

--- project/index.js
+++ project/index.js
@@ -6,6 +6,7 @@ const output = readline.createInterface({
   output: process.stdout,
 });
 
+//Inicial o programa
 menu();
 
 function menu(){
@@ -21,7 +22,8 @@ function menu(){
     [5] Sair
     ------------------------------------
     `,
-    function (option) {
+    //Aguarda a resposta de option e executa a respectiva função
+     option => {
       switch (option) {
         case "1":
           console.clear();
@@ -40,10 +42,12 @@ function menu(){
           option4();
           break;
         case "5":
+          //Caso aperte 5, o metodo close encerra o programa
           console.clear();
           output.close();
           break;
         default:
+          //Casp padrao, caso seja colocado um valor diferente do esperado
           console.clear();
           console.log("\n ## Opção inválida ##");
           menu();
@@ -53,9 +57,10 @@ function menu(){
 };
 
 function option1(){
+  //Manda uma mensagem no console e aguarda, dependendo da resposta realiza uma ação
   output.question(
     "\n [1] Voltar ao menu.\n ======================= \n Informe o ano de pesquisa: ",
-    (year) => {
+    year => {
       if (year == "1") {
         console.clear();
         menu();
@@ -70,13 +75,13 @@ function option1(){
 function option2(){
   output.question(
     "\n [1] Voltar ao menu.\n ------------------------------------ \n Informe o nome para pesquisa: ",
-    (nome) => {
+    name => {
       let item;
 
-      if (nome != "") {
-        item = methods.criptName(nome);
+      if (name != "") {
+        item = methods.criptName(name);
       }
-      if (nome == "1") {
+      if (name == "1") {
         console.clear();
         menu();
       }
@@ -88,12 +93,12 @@ function option2(){
 function option3(){
   output.question(
     "\n [1] Voltar ao menu.\n ======================= \n Informe o ano de pesquisa: ",
-    (ano) => {
-      if (ano == "1") {
+    year => {
+      if (year == "1") {
         console.clear();
         menu();
       } else {
-        let item = methods.averageYear(ano);
+        let item = methods.averageYear(year);
         verifyCases(item, option3);
       }
     }
@@ -103,7 +108,7 @@ function option3(){
 function option4(){
   output.question(
     "\n [1] Voltar ao menu.\n ======================= \n Pressione qualquer tecla para os resultados: ",
-    (reponse) => {
+    reponse => {
       if (reponse == "1") {
         console.clear();
         menu();
@@ -116,11 +121,13 @@ function option4(){
 }
 
 function verifyCases(returnItem, callback){
+  //Verifica se o returnItem é undefined ou não, para ver se foi digitado um dado esperado
   if (returnItem == undefined) {
     console.clear();
     console.log("\n ## Não foi localizado nenhum item ou dado inválido ##");
     callback();
   } else {
+  //Se não for, mostra o resultado e chama a o callback que nesse caso será a opção de digitar outro valor ou voltar a função passada como parametro
     console.clear();
     console.log("------------------------------------------------------------");
     console.log(returnItem);
